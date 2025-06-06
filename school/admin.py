a8bb6954bbcd4baf371066b12fc4a828bcd939f5
luis-navarro <inluisantonio2.2@gmail.com>
2025-06-06 00:25:44 -0500

    v4.0 Ajustes adicionales en admin.py, probar API en postman y clonar repositorio en un sistema diferente.

--- school/admin.py
+++ school/admin.py
@@ -2,6 +2,25 @@ from django.contrib import admin
 from .models import Cursos, Horarios, Generos, Estudiantes
 
 # Register your models here.
+@admin.register(Cursos)
+class CursosAdmin(admin.ModelAdmin):
+    list_display = ('nombre',)
+
+@admin.register(Horarios)
+class HorariosAdmin(admin.ModelAdmin):
+    list_display = ('jornada',)
+
+@admin.register(Generos)
+class GenerosAdmin(admin.ModelAdmin):
+    list_display = ('genero',)
+
+@admin.register(Estudiantes)
+class EstudiantesAdmin(admin.ModelAdmin):
+    list_display = ('nombres', 'apellidos', 'genero', 'correo', 'edad', 'telefono', 'curso', 'horario', 'activo', 'fecha_registro')
+    readonly_fields = ('fecha_registro',)
+
+
+'''
 class EstudianteAdmin(admin.ModelAdmin): 
     readonly_fields = ('fecha_registro',)
 
@@ -9,3 +28,4 @@ admin.site.register(Cursos)
 admin.site.register(Horarios)
 admin.site.register(Generos)
 admin.site.register(Estudiantes, EstudianteAdmin)
+'''
